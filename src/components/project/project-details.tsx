48caec0a8a63b9e7aafe2ff21d1966dbce3dd651
jagannathgouda2000 <jagannnathgouda@gmail.com>
2024-03-25 12:03:55 +0530

    changes in prodcut view

--- src/components/project/project-details.tsx
+++ src/components/project/project-details.tsx
@@ -3,13 +3,36 @@ import React from 'react'
 import Header from '../commonItems/Header';
 import { Card, CardContent } from '../ui/card';
 import ProjectInfo from './projectInfo';
+import { Button } from '../ui/button';
 
 const ProjectDetails = ({ projectId }: { projectId: string }) => {
     const { data: projectData } = api.project.getProjectById.useQuery({ id: projectId });
-    console.log(projectData,"project data")
+    console.log(projectData, "project data")
     return (
         <>
-            <Header title='Project Details' subtitle='View the details of the project.' />
+            <div className="mb-4 flex flex-col md:mb-8 md:flex-row md:items-center md:justify-between">
+                <Header title='Project Details' subtitle='View the details of the project.' />
+                <div className="flex items-center gap-4">
+                    <Button
+                        // onClick={() => {
+                        //     if (userName) {
+                        //         setName(userName);
+                        //         setIsEditing(false);
+                        //     }
+                        // }}
+                        className="grow px-8 md:grow-0"
+                    >
+                        <span className="font-bold">Edit project</span>
+                    </Button>
+                    <Button
+                        //onClick={handleProfileChanges}
+                        className="grow px-8 md:grow-0"
+                    >
+                        <span className="font-bold">Add Task</span>
+                    </Button>
+                </div>
+            </div>
+
             {!projectData && (
                 <div className="mb-10 mt-4 md:mb-20 text-center text-sm text-gray-400">
                     No project found.
@@ -18,18 +41,23 @@ const ProjectDetails = ({ projectId }: { projectId: string }) => {
             {projectData && (
                 <div className='mb-10 mt-4 md:mb-20'>
                     <ProjectInfo title={projectData?.title} description={projectData?.description} members={projectData.members} />
+                    <div>
+                        <h1 className="mb-2 text-2xl font-medium">Tasks</h1>
+                        <p className="text-md font-medium text-gray-400">Tasks in the projects</p>
+                    </div>
                     {projectData?.tasks.length > 0 && (
                         <>
-                            <div>
-                                <h1 className="mb-2 text-2xl font-medium">Tasks</h1>
-                                <p className="text-md font-medium text-gray-400">Tasks in the projects</p>
-                            </div>
                             {projectData.tasks.map((task) => {
                                 return (
                                     <p>hello</p>
                                 )
                             })}
                         </>
+                    )}
+                    {projectData?.tasks.length == 0 && (
+                        <div className="mb-10 mt-4 md:mb-20 text-center text-sm text-gray-400">
+                            No tasks found.
+                        </div>
                     )}
                 </div>
             )}
--- src/components/project/projectInfo.tsx
+++ src/components/project/projectInfo.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Card, CardContent } from '../ui/card'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip'
+import Image from 'next/image'
 
 const ProjectInfo = ({ title, description, members }: { title: string, description: string | null, members: any }) => {
     return (
@@ -12,6 +14,33 @@ const ProjectInfo = ({ title, description, members }: { title: string, descripti
                     <p className="text-sm text-muted-foreground">
                         {description}
                     </p>
+
+                    {members.length > 0 && (
+                        <div className="my-2 flex items-center gap-4">
+                            {members.map((k: any) => {
+                                return (
+                                    <TooltipProvider>
+                                        <Tooltip>
+                                            <TooltipTrigger asChild>
+                                                <Image
+                                                    key={k.id}
+                                                    alt=""
+                                                    width={48}
+                                                    height={48}
+                                                    src={k.image}
+                                                    className="h-8 w-8 rounded-full"
+                                                />
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                <p>{k.email}</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                )
+                            })}
+                        </div>
+                    )}
+
                 </div>
             </CardContent>
         </Card>
