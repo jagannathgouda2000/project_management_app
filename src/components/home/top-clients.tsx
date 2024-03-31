3696e5a679482197f3bb542beab9967c5f9dade8
jagannathgouda2000 <jagannnathgouda@gmail.com>
2024-03-31 18:11:13 +0530

    changes

--- src/components/home/top-clients.tsx
+++ src/components/home/top-clients.tsx
@@ -12,28 +12,33 @@ import Image from "next/image"
 
 export function TopClients() {
   return (
-    <Carousel
-      opts={{
-        align: "start",
-        loop : true
-      }}
-      className="w-full m-4"
-    >
-      <CarouselContent>
-        {Array.from({ length: 7 }).map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex items-center h-[80px] justify-center p-6">
-                    <Image alt="" src={`/landing-page/${index+1}.svg`} width={134} height={30} />
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <>
+      <div className='text-center text-4xl font-bold'>
+        Well-versed in all your most-used programs.
+      </div>
+      <Carousel
+        opts={{
+          align: "start",
+          loop: true
+        }}
+        className="w-full m-4 mt-20"
+      >
+        <CarouselContent>
+          {Array.from({ length: 7 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex items-center h-[80px] justify-center p-6">
+                    <Image alt="" src={`/landing-page/${index + 1}.svg`} width={134} height={30} />
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </>
   )
 }
